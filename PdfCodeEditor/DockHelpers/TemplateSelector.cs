cfaf81e66e9b953dac99b14660adefab238cc8ef
dmitry.goryachev <gdv1811@gmail.com>
2022-09-21 00:32:38 +0300

    Migrated to .net 6

---
+++ PdfCodeEditor/DockHelpers/TemplateSelector.cs
@@ -0,0 +1,40 @@
+﻿using AvalonDock.Layout;
+using System.Windows.Controls;
+using System.Windows;
+using PdfCodeEditor.ViewModels;
+
+namespace PdfCodeEditor.DockHelpers
+{
+    internal class TemplateSelector : DataTemplateSelector
+    {
+        public TemplateSelector()
+        {
+        }
+
+
+        public DataTemplate PdfDocumentViewTemplate
+        {
+            get;
+            set;
+        }
+
+        //public DataTemplate FileStatsViewTemplate
+        //{
+        //    get;
+        //    set;
+        //}
+
+        public override System.Windows.DataTemplate SelectTemplate(object item, System.Windows.DependencyObject container)
+        {
+            var itemAsLayoutContent = item as LayoutContent;
+
+            if (item is PdfDocumentViewModel)
+                return PdfDocumentViewTemplate;
+
+            //if (item is FileStatsViewModel)
+            //    return FileStatsViewTemplate;
+
+            return base.SelectTemplate(item, container);
+        }
+    }
+}
